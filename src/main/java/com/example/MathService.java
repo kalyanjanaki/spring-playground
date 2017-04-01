50c6958004c21cacb152f91108792147aa323be0
trainer20 <trainer20@trainer20s-MacBook-Pro.local>
2017-04-01 15:19:43 -0600

    Added Exercise MAth Form Data Area

--- src/main/java/com/example/MathService.java
+++ src/main/java/com/example/MathService.java
@@ -1,10 +1,12 @@
 package com.example;
 
+import com.sun.org.apache.regexp.internal.RE;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by trainer20 on 3/30/17.
@@ -50,5 +52,35 @@ public class MathService {
     public String  returnVolume(@PathVariable("l") int length,@PathVariable("w") int width,@PathVariable("h") int height){
 
         return String.format("The volume of a %dx%dx%d rectangle is %d",length,width,height,(length*width*height));
+    }
+
+    @PostMapping("/math/area")
+    public String returnArea(@RequestParam Map<String,String> params){
+
+        Double Areac;
+        Integer Arear;
+        String Result;
+        String type = params.get("type");
+        int radius ;
+        int width;
+        int height;
+
+        switch (type){
+            case "circle":
+                radius = Integer.parseInt(params.get("radius"));
+                Areac = Math.PI * radius * radius;
+                Result = String.format("Area of a circle with a radius of %d is %g",radius,Areac);
+                break;
+            case "rectangle":
+                width = Integer.parseInt(params.get("width"));
+                height = Integer.parseInt(params.get("height"));
+                Arear = width * height;
+                Result = String.format("Area of a %dx%d rectangle is %d",width,height,Arear);
+                break;
+            default:
+                Result = "Invalid";
+        }
+        return Result;
+
     }
 }
--- src/test/java/com/example/MathServiceTest.java
+++ src/test/java/com/example/MathServiceTest.java
@@ -4,11 +4,17 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
  * Created by trainer20 on 3/30/17.
@@ -45,6 +51,19 @@ public class MathServiceTest {
         this.mvc.perform(request)
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.content().string("The volume of a 3x4x5 rectangle is 60"));
+
+    }
+
+    @Test
+    public void testArea() throws Exception{
+        MockHttpServletRequestBuilder request = post("/math/area")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("type","rectangle")
+                .param("width","3")
+                .param("height","4");
+        this.mvc.perform(request)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string("Area of a 3x4 rectangle is 12"));
 
     }
 }
